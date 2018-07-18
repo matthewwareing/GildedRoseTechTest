d820fc037fe3c50e8f21489c6b9d1b6f4e2e18ad
Matthew <mdwareing@gmail.com>
2018-07-18 20:51:44 +0100

    Add decrementing quality 2x after sell by date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,11 +7,17 @@ describe GildedRose do
       gildedrose = GildedRose.new([chicken])
       expect { gildedrose.update_quality }.to change { gildedrose.items[0].quality }.by -1
     end
+    
     it 'decreases the sell-in by amount to decrement' do
       chicken = ItemWrapper.item(name: 'Chicken', sell_in: 5, quality: 2)
       gildedrose = GildedRose.new([chicken])
-      gildedrose.update_quality
-      expect(gildedrose.items[0].sell_in).to eq(4)
+      expect{gildedrose.update_quality}.to change {gildedrose.items[0].sell_in}.by -1
+    end
+
+    it 'decreases the quality, twice as fast, after sell by date' do
+        nickelback = ItemWrapper.item(name:"Nickelback", sell_in:0, quality:10)
+        gildedrose = GildedRose.new([nickelback])
+        expect{gildedrose.update_quality}.to change {gildedrose.items[0].quality}.by -2
     end
   end
 end
