8cfbbf8e6b1b0509cd79e4b2022706aeec1504db
Matthew <mdwareing@gmail.com>
2018-07-18 20:27:01 +0100

    Add decrementing quality test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,5 +2,11 @@ require 'gilded_rose.rb'
 
 describe GildedRose do
   describe '#update_quality' do
+    it 'decreases the quality by amount to decrement' do
+      chicken = ItemWrapper.item(name: 'Chicken', sell_in: 5, quality: 2)
+      gildrose = GildedRose.new([chicken])
+      gildrose.update_quality
+      expect(gildrose.items[0].quality).to eq 1
+    end
   end
 end
