be0e8fe6d831c455d655f47b95df8816c25f51d8
Matthew <mdwareing@gmail.com>
2018-07-18 20:40:48 +0100

    Add decrementing sell-in amount

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,9 +4,14 @@ describe GildedRose do
   describe '#update_quality' do
     it 'decreases the quality by amount to decrement' do
       chicken = ItemWrapper.item(name: 'Chicken', sell_in: 5, quality: 2)
-      gildrose = GildedRose.new([chicken])
-      gildrose.update_quality
-      expect(gildrose.items[0].quality).to eq 1
+      gildedrose = GildedRose.new([chicken])
+      expect { gildedrose.update_quality }.to change { gildedrose.items[0].quality }.by -1
+    end
+    it 'decreases the sell-in by amount to decrement' do
+      chicken = ItemWrapper.item(name: 'Chicken', sell_in: 5, quality: 2)
+      gildedrose = GildedRose.new([chicken])
+      gildedrose.update_quality
+      expect(gildedrose.items[0].sell_in).to eq(4)
     end
   end
 end
