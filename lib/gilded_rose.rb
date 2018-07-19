2a34cd66ac8787ca873665c1450779fb6ff0103e
Matthew <mdwareing@gmail.com>
2018-07-19 16:32:52 +0100

    Refactor methods to take edge cases out of main methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -35,7 +35,7 @@ class GildedRose
 
   def update_sell_in(item)
     item.sell_in -= 1
-    
+
     if item.sell_in < 0
       if item.name != 'Aged Brie'
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
@@ -51,15 +51,29 @@ class GildedRose
     end
   end
 
+  def default_updates(item)
+    if item.sell_in <= 0
+      decrease_quality(item, 2)
+    else
+      decrease_quality(item, 1)
+    end
+    item.sell_in -= 1
+  end
+
   def update_product
     @items.each do |item|
       if item.name != 'Sulfuras, Hand of Ragnaros'
-        update_quality(item) 
+        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+          default_updates(item)
+        else
+        update_quality(item)
         update_sell_in(item)
+        end
       end
     end
   end
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
